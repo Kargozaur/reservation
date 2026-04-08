2179d7725f43f8d744de71173701a30b1c0f966d
Illia <lpodildaviduk@gmail.com>
2026-04-08 15:37:32 +0200

    .

--- services/user-service/schemas/response/error_reponse.go
+++ services/user-service/schemas/response/error_reponse.go
@@ -3,3 +3,7 @@ package response
 type ErrorResponse struct {
 	Message string `json:"message"`
 }
+
+func ToErrorResponse(err error) *ErrorResponse {
+	return &ErrorResponse{Message: err.Error()}
+}
