0ba7004ae0c96dd46262a6e8b49db6c66964f478
Illia <lpodildaviduk@gmail.com>
2026-04-09 23:53:37 +0200

    .

--- services/user-service/middleware/ext.go
+++ services/user-service/middleware/ext.go
@@ -12,7 +12,7 @@ func extractToken(ctx *gin.Context) (string, error) {
 		return token, nil
 	}
 	auth := ctx.GetHeader("Authorization")
-	if token, ok := strings.CutPrefix(auth, "Bearer "); ok {
+	if token, ok := strings.CutPrefix(auth, "Bearer "); ok && token != "" {
 		return token, nil
 	}
 	return "", errors.New("Token is not provided")
