773335038b5b7404b997165dd92184fdfa8f47cb
Illia <lpodildaviduk@gmail.com>
2026-04-11 19:41:23 +0200

    swapped image, adjusted database creation

--- docker/init.sh
+++ docker/init.sh
@@ -6,7 +6,30 @@ done
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d postgres <<'SQL'
 
-CREATE DATABASE user_service;
-CREATE DATABASE reservation_service;
+CREATE EXTENSION IF NOT EXISTS dblink;
+
+DO
+$do$
+BEGIN
+   IF EXISTS (SELECT FROM pg_database WHERE datname = 'user_service') THEN
+      RAISE NOTICE 'Database already exists';
+   ELSE
+      PERFORM dblink_exec('dbname=' || current_database()
+                        , 'CREATE DATABASE user_service');
+   END IF;
+END
+$do$;
+
+DO
+$do$
+BEGIN
+   IF EXISTS (SELECT FROM pg_database WHERE datname = 'reservation_service') THEN
+      RAISE NOTICE 'Database already exists';
+   ELSE
+      PERFORM dblink_exec('dbname=' || current_database()
+                        , 'CREATE DATABASE reservation_service');
+   END IF;
+END
+$do$;
 
 SQL
