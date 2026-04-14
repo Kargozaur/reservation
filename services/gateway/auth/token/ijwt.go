6ab377d61a58399019a8b735cd511e53d3955d2c
Illia <lpodildaviduk@gmail.com>
2026-04-14 22:53:54 +0200

    cleanup

--- services/gateway/auth/token/ijwt.go
+++ services/gateway/auth/token/ijwt.go
@@ -3,8 +3,6 @@ package token
 import "github.com/google/uuid"
 
 type IJWT interface {
-	CreateAccessToken(userID uuid.UUID) (string, error)
-	CreateRefreshToken(userID uuid.UUID) (string, error)
 	VerifyToken(tokenString string) bool
 	GetUId(tokenString string) (uuid.UUID, error)
 }
